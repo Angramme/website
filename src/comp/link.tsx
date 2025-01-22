d191555a6ddb2e4aaae5d78cded82f99324514c5
Angramme <34948473+Angramme@users.noreply.github.com>
2025-01-22 02:16:46 +0100

    maybe fix

--- src/comp/link.tsx
+++ src/comp/link.tsx
@@ -4,30 +4,40 @@ import Link from 'next/link';
 import ArrowOutwardIcon from '@mui/icons-material/ArrowOutward';
 import { useMemo } from 'react';
 
-export default function Llink({ href, onClick, target, children, disabled }: Readonly<{ onClick?: () => void, href?: string, children: React.ReactNode, target?: string, disabled?: boolean}>) {    
-    if(href && onClick) throw new Error('Llink: cannot have both href and onClick');
-    const inside = useMemo(()=><>
-        <ArrowOutwardIcon className={`text-[var(${disabled ? '--foreground' : '--accent'})] group-hover:text-[var(--foreground)]`} />
+
+function Inner({ children, disabled }: Readonly<{ children: React.ReactNode, disabled?: boolean }>) {
+    return (
+        <>
+            <ArrowOutwardIcon className={`text-[var(${disabled ? '--foreground' : '--accent'})] group-hover:text-[var(--foreground)]`} />
             <span className='bg-black/5 dark:bg-white/5'>
                 {children}
             </span>
-    </>, [children, disabled]);
+        </>
+    );
+}
+
+export default function Llink({ href, onClick, target, children, disabled }: Readonly<{ onClick?: () => void, href?: string, children: React.ReactNode, target?: string, disabled?: boolean }>) {
+    if (href && onClick) throw new Error('Llink: cannot have both href and onClick');
     return (
         href ?
-        <Link
-            href={href}
-            onClick={onClick}
-            target={target}
-            className={`group ${disabled ? 'opacity-60' : 'hover:bg-[var(--accent)]'}`}
-        >
-            {inside}
-        </Link>
-        :
-        <span
-            onClick={onClick}
-            className={`group ${disabled ? 'opacity-60' : 'hover:bg-[var(--accent)]'}`}
-        >
-            {inside}
-        </span>
+            <Link
+                href={href}
+                onClick={onClick}
+                target={target}
+                className={`group ${disabled ? 'opacity-60' : 'hover:bg-[var(--accent)]'}`}
+            >
+                <Inner disabled={disabled}>
+                    {children}
+                </Inner>
+            </Link>
+            :
+            <span
+                onClick={onClick}
+                className={`group ${disabled ? 'opacity-60' : 'hover:bg-[var(--accent)]'}`}
+            >
+                <Inner disabled={disabled}>
+                    {children}
+                </Inner>
+            </span>
     );
 }
